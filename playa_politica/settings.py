c3b6c0240b0ccb9661016e78bb4a457bd3c87a35
Patrick G <patdg@pm.me>
2022-05-25 21:02:25 -0700

    Added static root and additional allowed host.

--- playa_politica/settings.py
+++ playa_politica/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9tpyb5s8ye==+ysluixbk+01*6g4eo!!7wxkt#+sfdmdl$+s^j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
